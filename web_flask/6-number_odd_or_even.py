87fa6db33836e3cd8f38ccb1d0b1c4c33f4b8c5c
katman-Generation <tkatema5@gmail.com>
2022-10-16 11:57:02 -0700

    submitting

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,6 @@ def number_template(n):
         return render_template('5-number.html', n=n)
 
 
-
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """Returns a template at the /number_odd_or_even/<n>
